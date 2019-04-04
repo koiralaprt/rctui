ca625aec536d54edb480e58f01087729858d1bf5
koiralaprt <prayatna@bottle.com.np>
2019-04-04 17:44:54 +0545

    button styling

--- src/lib/button/index.js
+++ src/lib/button/index.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-
+import './style.scss';
 export default class Button extends Component {
   render() {
     return (
-      <div className="" onClick={this.props.onClick}>
+      <button className="rct-btn" onClick={this.props.onClick}>
         {this.props.value}
-      </div>
+      </button>
     );
   }
 }
