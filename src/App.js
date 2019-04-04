6fdc4240835ea521c42fe91cd86ba6a9863791a1
koiralaprt <prayatna@bottle.com.np>
2019-04-04 17:40:31 +0545

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from './lib/button';
 
 class App extends Component {
   render() {
@@ -19,6 +20,7 @@ class App extends Component {
           >
             Learn React
           </a>
+          <Button value="aa" />
         </header>
       </div>
     );
